46ce2cac19139f2ac867781d91e6a63cb7440099
Alvin Galit <aroblesgalit@gmail.com>
2018-12-16 14:35:05 -0800

    check for winner and check for tie, added reset and new game

--- js/script.js
+++ js/script.js
@@ -7,12 +7,14 @@ let playerTwo = {
 }
 
 const Game = () => {
+    // when clicking button "player x"
     document.getElementById('btn-playerx').addEventListener('click', function() {
         document.getElementById('board').style.display="flex";
         document.getElementById('playerPanel').style.display="none";
         playerOne.mark = 'X';
         playerTwo.mark = '0';
     });
+    // when clicking button "player o"
     document.getElementById('btn-player0').addEventListener('click', function() {
         document.getElementById('board').style.display="flex";
         document.getElementById('playerPanel').style.display="none";
@@ -20,10 +22,40 @@ const Game = () => {
         playerTwo.mark = 'X';
     });
 
+    // when clicking button "New Game"
+    document.getElementById('btn-nw-game').addEventListener('click', function() {
+        Array.prototype.forEach.call(document.querySelectorAll('.grid-item'), gridItem => {
+            gridItem.innerHTML='';
+        });
+        document.getElementById('message-game').style.display="none";
+    });
 
+    // when clicking button "Restart"
+    document.getElementById('btn-reset-game').addEventListener('click', function() {
+        window.location.reload();
+    });
+
+    const displayWinner = () => {
+        if(playerTurn) {
+            document.getElementById('message-game').style.display="flex";
+            document.getElementById('message').innerHTML = `Player ${playerTwo.mark} wins!`;
+        } else {
+            document.getElementById('message-game').style.display="flex";
+            document.getElementById('message').innerHTML = `Player ${playerOne.mark} wins!`;
+        }   
+    }
+
+    const checkForTie = () => {
+        // check to see if all cells have x's and o's but no winner
+        if (board.length === 9) {
+            document.getElementById('message-game').style.display="flex";
+            document.getElementById('message').innerHTML = "It's a tie!";
+        }
+
+    }
 
     const checkWinner = () => {
-        const board = [];
+        let board = [];
         board.push(document.getElementById("cell-one").innerHTML);
         board.push(document.getElementById("cell-two").innerHTML);
         board.push(document.getElementById("cell-three").innerHTML);
@@ -33,51 +65,37 @@ const Game = () => {
         board.push(document.getElementById("cell-seven").innerHTML);
         board.push(document.getElementById("cell-eight").innerHTML);
         board.push(document.getElementById("cell-nine").innerHTML);
-        if (board[0].length) {
-            if (board[0] === board[1]) {
-                if (board[1] === board[2]) {
-                    console.log("Winning!");
-                }                      
-            } else if (board[0] === board[3]) {
-                if (board[3] === board[6]) {
-                    console.log('Winner!');
-                }
-            } 
-            else if (board[0] === board[4]) {
-                if (board[4] === board[8]) {
-                    console.log('Winner!');
-                }
+        if (board[0] !== '') {
+            if (board[0] === board[1] && board[1] === board[2]) {
+                displayWinner();
+            } else if (board[0] === board[3] && board[3] === board[6]) {
+                displayWinner();
+            } else if (board[0] === board[4] && board[4] === board[8]) {
+                displayWinner();
             }
-        } else if (board[4] != '') {
-            if (board[4] === board[3]) {
-                if (board[3] === board[5]) {
-                    console.log('Winning!');
-                }
-            } else if (board[4] === board[1]) {
-                if (board[1] === board[7]) {
-                    console.log('Winning!');
-                }
-            } else if (board [4] === board[2]) {
-                if (board[2] === board[6]) {
-                    console.log('Winning!');
-                }
+            return;
+        }
+        if (board[4] !== '') {
+            if (board[4] === board[3] && board[3] === board[5]) {
+                displayWinner();
+            } else if (board[4] === board[1] && board[1] === board[7]) {
+                displayWinner();
+            } else if (board [4] === board[2] && board[2] === board[6]) {
+                displayWinner();
             }
-         } 
-        //  
-        //     } 
-        // } else if (board[8] != '') {
-        //     if (board[8] === board[7]) {
-        //         if (board[7] === board[6]) {
-        //             console.log('Winning!');
-        //         }
-        //     } else if (board[8] === board[5]) {
-        //         if (board[5] === board[2]) {
-        //             console.log('Winning!');
-        //         }
-        //     }
-        // }
-        // }
+            return;
+        }      
+        if (board[8] !== '') {
+            if (board[8] === board[7] && board[7] === board[6]) {
+                displayWinner();
+            } else if (board[8] === board[5] && board[5] === board[2]) {
+                displayWinner();
+            }
+            return;
+        }
+        
     }
+
 
     let playerTurn = true;
       
@@ -96,9 +114,8 @@ const Game = () => {
             // if 3 in a row -- winner message
             // if no winner -- next player turn     
         }
-        checkWinner();   
-
-        
+        checkWinner();
+        checkForTie();
     });
 };
   
